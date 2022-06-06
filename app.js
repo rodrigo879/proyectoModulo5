54f9508a4aa5e2a80a9a69cff659c66dcbb2dd97
Rodrigo Alvarez <ralvarez@lequipe.net>
2022-06-06 09:20:24 -0300

    proyecto avanzado

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,'./public')));
+
+app.get('/', function(req,res){
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+})
+
+app.listen(3000, function(){
+    console.log("Servidor corriendo en puerto 3000");
+})

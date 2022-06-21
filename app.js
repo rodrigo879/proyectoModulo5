e921f001f688b1f5aa89a2929c4eeb7d94fa5e72
Rodrigo Alvarez <rjalvarez_879@hotmail.com>
2022-06-21 20:11:11 -0300

    para subir a heroku

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
+
 app.use(express.static(path.resolve(__dirname,'./public')));
 
 app.get('/', function(req,res){
@@ -20,6 +23,6 @@ app.get('*', function(req,res){
     res.sendFile(path.resolve(__dirname,'./views/404.html'))
 })
 
-app.listen(3000, function(){
-    console.log("Servidor corriendo en puerto 3000");
+app.listen(PORT, function(){
+    console.log(`Server running at http://${HOST}:${PORT}/`);
 })
